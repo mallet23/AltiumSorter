6033d1b1890a78a55f92ef37dbb71ad64cabe916
Artur Salakhov <gsmilexd@gmail.com>
2020-09-07 00:19:03 +0300

    init

---
+++ AltiumSorter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AltiumSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
